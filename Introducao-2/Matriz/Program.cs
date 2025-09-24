bd63d0150c5997d3840ccfd20beaa2fc47f78d6e
Caio Arasaki <sasazaki08@gmail.com>
2025-09-24 02:34:32 -0300

    Att

---
+++ Introducao-2/Matriz/Program.cs
@@ -0,0 +1,38 @@
+﻿string[] fraudulentOrderIds = ["A123", "B456", "C789"];
+
+// fraudulentOrderIds[0] = "A123";
+// fraudulentOrderIds[1] = "B456";
+// fraudulentOrderIds[2] = "C789";
+
+Console.WriteLine($"First: \t{fraudulentOrderIds[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIds[1]}");
+Console.WriteLine($"Third: \t{fraudulentOrderIds[2]}\n");
+
+fraudulentOrderIds[0] = "E321";
+fraudulentOrderIds[1] = "F654";
+fraudulentOrderIds[2] = "G987";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIds[0]}");
+Console.WriteLine($"Reassign Second: {fraudulentOrderIds[1]}");
+Console.WriteLine($"Reassign Third: {fraudulentOrderIds[2]}\n");
+
+Console.WriteLine($"There are {fraudulentOrderIds.Length} fraudulent orders to process.\n");
+
+string[] names = { "Caio", "Camila", "Carina" };
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+int[] inventory = [200, 450, 750, 175, 250];
+int sum = 0;
+int bin= 0;
+
+foreach (int item in inventory)
+{
+    sum += item;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
--- Introducao-2/Teste/obj/Debug/net9.0/Teste.AssemblyInfo.cs
+++ Introducao-2/Teste/obj/Debug/net9.0/Teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3322992476be8ae231671ed9540d3710fb07da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be412a8720b4789f18e1f4a27666421347cfa0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
