fd672714b7cffceba65d96f0721a0ad88fd6038d
Caio Arasaki <sasazaki08@gmail.com>
2025-09-25 02:01:21 -0300

    Att

--- Introducao-2/Learn/Game/obj/Debug/net9.0/Game.AssemblyInfo.cs
+++ Introducao-2/Learn/Game/obj/Debug/net9.0/Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd63d0150c5997d3840ccfd20beaa2fc47f78d6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798c29e03e22c130d611a0d8ede03a9a82f93214")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
