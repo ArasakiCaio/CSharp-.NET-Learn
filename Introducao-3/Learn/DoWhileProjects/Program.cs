8903d045e0ca5929a4cb6b5f183655f51779e04d
Caio Arasaki <sasazaki08@gmail.com>
2025-11-08 00:06:04 -0300

    Projetos de com Do-While (1/3)

---
+++ Introducao-3/Learn/DoWhileProjects/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Insira um inteiro de 5 a 10: ");
+string? input;
+int value = 0;
+bool flag = false;
+
+do
+{
+    input = Console.ReadLine();
+
+    flag = int.TryParse(input, out value);
+
+    if (flag == false)
+    {
+        Console.Write("A entrada inserida não é um inteiro. Insira um inteiro: ");
+    }
+    else
+    {
+        if (!(value >= 5 && value <= 10))
+        {
+            Console.Write("O número inserido não esta entre 5 e 10. Insira outro inteiro: ");
+        }
+    }
+} while (flag == false || (value >= 5 && value <= 10) == false);
+
+Console.WriteLine("Entrada aceita.");
